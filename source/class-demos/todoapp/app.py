1062d41753a43b0b6b1105a1a10afe6cdf3249ca
Robin Smorenburg <robin@smorenburg.io>
2021-03-15 07:08:04 +0100

    Add migration

--- source/class-demos/todoapp/app.py
+++ source/class-demos/todoapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -9,17 +10,17 @@ app.config[
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer(), primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean(), nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
-
-
-db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
